9bcdc8f70ee2c0582e93483fbeb52ca2035e9fc7
Bilal Mohammed <kurdo1ka@gmail.com>
2024-09-08 15:51:20 +0200

    added js, maked json array

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Welcher deutsche Fluss ist der längste?",
+        "answer_1": "Rhein",
+        "answer_2": "Elbe",
+        "answer_3": "Donau",
+        "answer_4": "Oder",
+        "right_answer": 1
+    },
+    {
+        "question": "In welchem Jahr wurde die deutsche Einheit gefeiert?",
+        "answer_1": "1989",
+        "answer_2": "1990",
+        "answer_3": "1991",
+        "answer_4": "1992",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches deutsche Bundesland ist flächenmäßig am größten?",
+        "answer_1": "Baden-Württemberg",
+        "answer_2": "Nordrhein-Westfalen",
+        "answer_3": "Niedersachsen",
+        "answer_4": "Bayern",
+        "right_answer": 4
+    },
+];
